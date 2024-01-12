c5a14e410f61815d64af6ec5fc2d3aabdcc9e03b
MestriRahulgithub <rahulmestrirahul@gmail.com>
2024-01-12 16:31:08 +0530

    Your commit message

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
-const Header = () => {
+const  Header = () => {
+    
     return (
-        <div>
-            <nav style={{backgroundColor:"lightgreen"}} class="navbar navbar-expand-lg bg-body-tertiary">
-            <a class="navbar-brand" href="navbar">Toursit Palces </a>
+        <div >
+            <nav style={{backgroundColor:"lightgrey"}}  class="navbar navbar-expand-lg bg-body-tertiary">
+            <a style={{background:"lightgrey"}}  class="navbar-brand" href="navbar">Toursit Palces </a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                <div class="container-fluid">
+                <div style={{backgroundColor:"lightgrey"}} class="container-fluid">
                     <div  class="collapse navbar-collapse justify-content-end
                     
                     " id="navbarNav">
-                        <ul class="navbar-nav">
+                        <ul style={{backgroundColor:"lightgray"}} class="navbar-nav">
                             <li class="nav-item">
                                 <a class="nav-link active" aria-current="page" href="/home">Home</a>
                             </li>
